3ce53716ead6c1a0f0b8d2f6140a534f0e8fec0f
ygglue <lagumbayeliyahu@gmail.com>
2026-07-01 17:25:32 +0800

    feat: add SEO meta, sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://YOUR_GITHUB_USERNAME.github.io',
   base: '/palawanlevelup',
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
